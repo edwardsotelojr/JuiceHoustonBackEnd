3c26bf4787d81108a8a0669426cb6ef1ffeb9052
Sir Edward <edwardsotelojr@gmail.com>
2022-05-04 22:12:24 -0500

    update index.js for production

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 require('dotenv').config()
+const path = require('path');
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
 const routes = require("./routes/");
 const bodyParser = require("body-parser")
 const express = require("express");
 const mongoose = require("mongoose");
 require('./models/Drink');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const PORT = process.env.PORT;
 const passport = require("passport");
-const cors = require('cors')
-
+const cors = require('cors');
+const options = {
+   cert: fs.readFileSync("/etc/letsencrypt/live/juicedhouston.com/cert.pem"),
+   key: fs.readFileSync("privkey.pem"),
+   ca: fs.readFileSync("/etc/letsencrypt/live/juicedhouston.com/chain.pem")
+}
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', (req, res) => {
+    res.send(path.join(__dirname, 'build', 'index.html'));
+});
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
 }).then(() => {
@@ -27,8 +40,9 @@ app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use('/', routes);
-app.listen(PORT, () => {
-  console.log("Server is running on Port: " + PORT);
+http.createServer(app).listen(8081, () => {
+   console.log("http server is running on Port: " + 8081);
+})
+https.createServer(options, app).listen(8080, () => {
+  console.log("httpsserver is running on Port: " + 8080);
 });
-
-
